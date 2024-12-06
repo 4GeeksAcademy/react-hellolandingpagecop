1b64f841483efff8c48e1425fc36ca94c43758a9
CamBombarda <osoriocamila@live.com>
2024-12-06 20:29:14 +0000

    ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
 import React from "react";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const cards = [
+	{
+		image: "https://picsum.photos/id/237/200/300",
+		title: "Puppy",
+		description: "A cute puppy",
+	},
+	{
+		image: "https://picsum.photos/id/7/4728/3168",
+		title: "A Meeting",
+		description: "A meeting in a coffe shop",
+	},
+	{
+		image: "https://picsum.photos/id/40/4106/2806",
+		title: "Cat",
+		description: "A cute cat",
+	},
+	{
+		image: "https://picsum.photos/id/82/1500/997",
+		title: "Flowers",
+		description: "Beautiful flowers",
+	}
+];
 
-//create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="container">
+				<div className="row">
+					{cards.map((item, index) => (
+						<div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
+							<Card cards={item} />
+						</div>
+					))}
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
